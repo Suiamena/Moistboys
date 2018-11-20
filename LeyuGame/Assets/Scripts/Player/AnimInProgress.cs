f8c42f82ad358cdf52fbe01ac73897233052b189
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-20 14:16:21 +0100

    commit
    commit

---
+++ LeyuGame/Assets/Scripts/Player/AnimInProgress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimInProgress : MonoBehaviour {
+
+    public Animator anim;
+
+	
+	void Update ()
+    {
+        RunAnimation();
+	}
+
+    void RunAnimation()
+    {
+        if (Input.GetButtonDown("A Button"))
+        {
+            Debug.Log("running");
+            anim.SetBool("IsLaunching", true);
+        }
+    }
+
+}
