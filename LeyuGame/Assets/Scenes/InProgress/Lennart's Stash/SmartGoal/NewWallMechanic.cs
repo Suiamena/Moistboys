a5322fc1f70f3fa751b31392dede11d6a4c79fc5
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-21 15:15:47 +0100

    Smart shit

--- LeyuGame/Assets/Scenes/InProgress/Lennart's Stash/SmartGoal/NewWallMechanic.cs
+++ LeyuGame/Assets/Scenes/InProgress/Lennart's Stash/SmartGoal/NewWallMechanic.cs
@@ -4,4 +4,22 @@ using UnityEngine;
 
 public class NewWallMechanic : MonoBehaviour {
 
+    public GameObject moustacheBoi;
+    public int creatureRange = 10;
+    public GameObject player;
+
+    private void FixedUpdate()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        //FOLLOW THE PLAYER
+        if (Vector3.Distance(moustacheBoi.transform.position, player.transform.position) < creatureRange)
+        {
+            Debug.Log("start");
+        }
+    }
+
 }
