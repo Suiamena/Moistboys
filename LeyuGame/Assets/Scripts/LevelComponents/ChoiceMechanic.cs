af1e267707e805a6d22dc2a2486dc55e9e0a9f27
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-22 12:34:07 +0100

    Keuzemoment Cutscene in progress
    Progress

--- LeyuGame/Assets/Scripts/LevelComponents/ChoiceMechanic.cs
+++ LeyuGame/Assets/Scripts/LevelComponents/ChoiceMechanic.cs
@@ -4,54 +4,174 @@ using UnityEngine;
 
 public class ChoiceMechanic : MonoBehaviour {
 
+    //Player Settings
     GameObject player;
-    GameObject moustacheBoiCutscene;
-    GameObject moustacheBoiTarget;
     GameObject playerCamera;
-    GameObject cutsceneCamera;
 
     PlayerController playerScript;
     Rigidbody playerRig;
 
-    bool cutsceneRunning;
+    //MoustacheBoi Settings
+    GameObject moustacheBoiCutscene;
+    GameObject moustacheBoiTarget;
 
-    int moustacheBoiSpeed = 10;
+    int moustacheBoiSpeed = 10, abilitySpeed = 3;
+
+    //WarmthSource Settings
+    GameObject warmthSource;
+    GameObject playerAbilityTarget;
+    public GameObject playerAbility, moustacheBoiAbility;
+    public int takeAbilityRange = 30;
+
+    //Cutscene Settings
+    public GameObject cutsceneCamera;
+
+    //cutscene 1
+    bool creatureMoves, abilityMoves, cutsceneFinished;
+
+    //cutscene 2
+    bool playerAbilityMoves, playerPushing, secondCutsceneFinished;
 
     private void Awake()
     {
         player = GameObject.Find("Character");
         playerScript = player.GetComponent<PlayerController>();
         playerRig = player.GetComponent<Rigidbody>();
+        playerCamera = GameObject.Find("Main Camera");
+
         moustacheBoiCutscene = GameObject.Find("MoustacheBoiCutscene");
         moustacheBoiTarget = GameObject.Find("MoustacheBoiTarget");
-        playerCamera = GameObject.Find("Main Camera");
-        cutsceneCamera = GameObject.Find("CutsceneCamera");
 
-        cutsceneCamera.SetActive(true);
+        warmthSource = GameObject.Find("WarmthSourceCutscene");
+        playerAbilityTarget = GameObject.Find("PlayerAbilityTarget");
     }
 
     private void FixedUpdate()
     {
-        if (cutsceneRunning)
+        //Cutscene
+        if (creatureMoves)
         {
-            StartCutscene();
+            CreatureMoves();
         }
+        if (abilityMoves)
+        {
+            CreatureAbilityMoves();
+        }
+        if (playerAbilityMoves)
+        {
+            PlayerAbilityMovesToSource();
+        }
+
+        //After cutscene
+        TakePlayerAbility();
     }
 
+    //SETUP CUTSCENE
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Player")
         {
-            cutsceneRunning = true;
+            if (!cutsceneFinished)
+            {
+                //stop player from moving
+                playerRig.velocity = new Vector3(0, 0, 0);
+                playerScript.enabled = false;
+                //set camera
+                cutsceneCamera.SetActive(true);
+                playerCamera.SetActive(false);
+
+                StartCoroutine(CreatureApproachesSource());
+            }
         }
     }
 
-    void StartCutscene()
+    IEnumerator CreatureApproachesSource()
     {
-        playerRig.velocity = new Vector3(0, 0, 0);
-        playerScript.enabled = false;
-        playerCamera.SetActive(false);
-        moustacheBoiCutscene.transform.position = Vector3.MoveTowards(moustacheBoiCutscene.transform.position, moustacheBoiTarget.transform.position, moustacheBoiSpeed * Time.deltaTime);
-        Debug.Log("start cutscene");
+        //PLAY CUTSCENE
+
+        //Creature Moves
+        creatureMoves = true;
+        yield return new WaitForSeconds(3F);
+        creatureMoves = false;
+
+        //Ability is Lost
+        moustacheBoiAbility.SetActive(true);
+        abilityMoves = true;
+        yield return new WaitForSeconds(2F);
+        abilityMoves = false;
+        yield return new WaitForSeconds(2F);
+
+        StopCutscene();
     }
+
+    void CreatureMoves()
+    {
+        moustacheBoiCutscene.transform.position = Vector3.MoveTowards(moustacheBoiCutscene.transform.position, moustacheBoiTarget.transform.position, moustacheBoiSpeed * Time.deltaTime);
+    }
+
+    void CreatureAbilityMoves()
+    {
+        moustacheBoiAbility.transform.position = Vector3.MoveTowards(moustacheBoiAbility.transform.position, new Vector3(moustacheBoiAbility.transform.position.x, moustacheBoiAbility.transform.position.y + 3, moustacheBoiAbility.transform.position.z), abilitySpeed * Time.deltaTime);
+    }
+
+    void StopCutscene()
+    {
+        playerScript.enabled = true;
+        playerCamera.SetActive(true);
+        cutsceneCamera.SetActive(false);
+        playerRig.velocity = new Vector3(0, 0, 0);
+        cutsceneFinished = true;
+    }
+
+
+    //AFTER CUTSCENE
+    void TakePlayerAbility()
+    {
+        if (Vector3.Distance(warmthSource.transform.position, player.transform.position) < takeAbilityRange)
+        {
+            if (!secondCutsceneFinished)
+            {
+                //stop player from moving
+                playerRig.velocity = new Vector3(0, 0, 0);
+                playerScript.enabled = false;
+
+                StartCoroutine(PlayerLosesAbility());
+            }
+        }
+    }
+
+    IEnumerator PlayerLosesAbility()
+    {
+        //PLAY CUTSCENE
+
+        //Ability moves
+        playerAbility.transform.position = player.transform.position;
+        playerAbility.SetActive(true);
+        playerAbilityMoves = true;
+        yield return new WaitForSeconds(3F);
+        playerAbilityMoves = false;
+
+        //Push player back
+        playerPushing = true;
+        yield return new WaitForSeconds(2F);
+        playerPushing = false;
+
+        StopSecondCutscene();
+    }
+
+    void PlayerAbilityMovesToSource()
+    {
+        //playerAbility.transform.position = Vector3.MoveTowards(playerAbility.transform.position, playerAbilityTarget.transform.position, abilitySpeed * Time.deltaTime);
+        //playerAbility.transform.position = Vector3.MoveTowards(playerAbility.transform.position, new Vector3(playerAbility.transform.position.x, playerAbility.transform.position.y + 3, playerAbility.transform.position.z), abilitySpeed * Time.deltaTime);
+        playerAbility.transform.position = Vector3.MoveTowards(playerAbility.transform.position, playerAbilityTarget.transform.position, abilitySpeed * Time.deltaTime);
+
+    }
+
+    void StopSecondCutscene()
+    {
+        playerScript.enabled = true;
+        playerRig.velocity = new Vector3(0, 0, 0);
+        secondCutsceneFinished = true;
+    }
+
 }
