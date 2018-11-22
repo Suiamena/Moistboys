74db8d05668877079543b699969a2e374c953f8a
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-22 11:13:15 +0100

    Cutscene part 2
    Work in progress

--- LeyuGame/Assets/Scripts/LevelComponents/ChoiceMechanic.cs
+++ LeyuGame/Assets/Scripts/LevelComponents/ChoiceMechanic.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class ChoiceMechanic : MonoBehaviour {
 
+    GameObject player;
     GameObject moustacheBoiCutscene;
-    GameObject warmthSourceCutscene;
+    GameObject moustacheBoiTarget;
+    GameObject playerCamera;
+    GameObject cutsceneCamera;
+
+    PlayerController playerScript;
+    Rigidbody playerRig;
+
+    bool cutsceneRunning;
 
     int moustacheBoiSpeed = 10;
 
     private void Awake()
     {
+        player = GameObject.Find("Character");
+        playerScript = player.GetComponent<PlayerController>();
+        playerRig = player.GetComponent<Rigidbody>();
         moustacheBoiCutscene = GameObject.Find("MoustacheBoiCutscene");
-        warmthSourceCutscene = GameObject.Find("WarmthSourceCutscene");
+        moustacheBoiTarget = GameObject.Find("MoustacheBoiTarget");
+        playerCamera = GameObject.Find("Main Camera");
+        cutsceneCamera = GameObject.Find("CutsceneCamera");
+
+        cutsceneCamera.SetActive(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (cutsceneRunning)
+        {
+            StartCutscene();
+        }
     }
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Player")
         {
-            moustacheBoiCutscene.transform.position = Vector3.MoveTowards(moustacheBoiCutscene.transform.position, warmthSourceCutscene.transform.position, moustacheBoiSpeed * Time.deltaTime);
-            Debug.Log("start cutscene");
+            cutsceneRunning = true;
         }
+    }
+
+    void StartCutscene()
+    {
+        playerRig.velocity = new Vector3(0, 0, 0);
+        playerScript.enabled = false;
+        playerCamera.SetActive(false);
+        moustacheBoiCutscene.transform.position = Vector3.MoveTowards(moustacheBoiCutscene.transform.position, moustacheBoiTarget.transform.position, moustacheBoiSpeed * Time.deltaTime);
+        Debug.Log("start cutscene");
     }
 }
