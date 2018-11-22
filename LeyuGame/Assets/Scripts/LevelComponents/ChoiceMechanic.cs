30046a65695e4eb3116db6b55c4c7f2a8b53f66c
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-22 10:58:00 +0100

    Cutscene Part 1
    Choice Cutscene In Progress

---
+++ LeyuGame/Assets/Scripts/LevelComponents/ChoiceMechanic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceMechanic : MonoBehaviour {
+
+    GameObject moustacheBoiCutscene;
+    GameObject warmthSourceCutscene;
+
+    int moustacheBoiSpeed = 10;
+
+    private void Awake()
+    {
+        moustacheBoiCutscene = GameObject.Find("MoustacheBoiCutscene");
+        warmthSourceCutscene = GameObject.Find("WarmthSourceCutscene");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            moustacheBoiCutscene.transform.position = Vector3.MoveTowards(moustacheBoiCutscene.transform.position, warmthSourceCutscene.transform.position, moustacheBoiSpeed * Time.deltaTime);
+            Debug.Log("start cutscene");
+        }
+    }
+}
