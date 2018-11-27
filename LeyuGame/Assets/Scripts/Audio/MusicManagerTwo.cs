32904fa251a36e2f16cf68f560e9cc7e195e4221
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-27 12:26:24 +0100

    Music Partialy implemented
    The current music is implemented, but is still has some troubles.

---
+++ LeyuGame/Assets/Scripts/Audio/MusicManagerTwo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagerTwo : MonoBehaviour
+{
+
+    [Header("fmod parameters")]
+    public float sound;
+
+    [FMODUnity.EventRef]
+    public string music = "event:/Music";
+    public FMOD.Studio.EventInstance Music;
+
+    public FMOD.Studio.ParameterInstance MusicParameter;
+
+    public int musicStage;
+
+    private void Awake()
+    {
+        //FMOD SETUP
+        Music = FMODUnity.RuntimeManager.CreateInstance(music);
+        Music.getParameter("Music", out MusicParameter);
+        Music.start();
+    }
+
+    private void FixedUpdate()
+    {
+        MusicParameter.setValue(sound);
+        sound += Mathf.Lerp(0f, 4.5f, 0.01f);
+        sound = Mathf.Clamp(sound, 0, 4.5f);
+    }
+
+}
