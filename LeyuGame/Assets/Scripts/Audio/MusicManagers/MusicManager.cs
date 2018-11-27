3285bf2a7c1c2362ca783288a206da05fbcf5504
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-27 15:33:29 +0100

    commit
    commit

--- LeyuGame/Assets/Scripts/Audio/MusicManagers/MusicManager.cs
+++ LeyuGame/Assets/Scripts/Audio/MusicManagers/MusicManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MusicManager : MonoBehaviour {
 
     [Header("fmod parameters")]
-    public float sound;
+    public float sound, launchSound;
 
     [FMODUnity.EventRef]
     public string music = "event:/Music";
@@ -17,44 +17,64 @@ public class MusicManager : MonoBehaviour {
     public FMOD.Studio.ParameterInstance LaunchParameter;
 
     public int musicStage;
+    bool playTutorialSound, playCreatureSound, playCoroutineOnce;
 
     private void Awake()
     {
         //FMOD SETUP
         Music = FMODUnity.RuntimeManager.CreateInstance(music);
+        Launch = FMODUnity.RuntimeManager.CreateInstance(launch);
         Music.getParameter("Music", out MusicParameter);
         Launch.getParameter("Launch", out LaunchParameter);
         Music.start();
-        Launch.start();
+        //Launch.start();
 
         //WAKE UP
-        //sound = 0.5f;
+        sound = 0.5f;
     }
 
     private void FixedUpdate()
     {
         RegulateMusic();
         MusicParameter.setValue(sound);
+        LaunchParameter.setValue(launchSound);
+
+        if (!playCoroutineOnce)
+        {
+            StartCoroutine(LaunchEffect());
+            playCoroutineOnce = true;
+        }
     }
 
     void RegulateMusic()
     {
         if (musicStage == 1)
         {
-            //BOUNCE TUTORIAL
-            //sound = 2.5f;
-
-            // laat los van loading bar
-            LaunchParameter.setValue(1f);
-            Debug.Log("ey");
+            if (!playTutorialSound)
+            {
+                //BOUNCE TUTORIAL
+                sound = 2.5f;
+                playTutorialSound = true;
+            }
         }
         if (musicStage == 2)
         {
-            //MEET CREATURE
-            sound = 3.5f;
-
-            //laad loading bar weer
-            LaunchParameter.setValue(0f);
+            if (!playCreatureSound)
+            {
+                //MEET CREATURE
+                sound = 3.5f;
+                playCreatureSound = true;
+            }
         }
+    }
+
+    IEnumerator LaunchEffect()
+    {
+        Launch.start();
+        //LOAD LAUNCH
+        launchSound = 0f;
+        yield return new WaitForSeconds(1F);
+        //FREE LAUNCH
+        launchSound = 1f;
     }
 }
