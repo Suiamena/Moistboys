1e02a0d0db236a27de088e86bdeceea7e34198da
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-29 10:31:26 +0100

    Level 1 Rough Done
    Level 1 rough version done + finish level 4 prefab.

--- LeyuGame/Assets/Scripts/GameArchitecture/LoadLevelTwo.cs
+++ LeyuGame/Assets/Scripts/GameArchitecture/LoadLevelTwo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MeetCreatureCutscene : MonoBehaviour {
+public class LoadLevelTwo : MonoBehaviour {
 
     GameObject creatureInSnow;
     public GameObject creatureOutSnow;
