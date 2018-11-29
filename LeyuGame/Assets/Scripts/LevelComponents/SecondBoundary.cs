dbd47714d052852e093f88421233bf2d6ab79bd9
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-29 15:37:08 +0100

    Boundaries in progress
    Commit

---
+++ LeyuGame/Assets/Scripts/LevelComponents/SecondBoundary.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondBoundary : MonoBehaviour {
+
+    //PLAYER
+    GameObject player;
+    PlayerController playerScript;
+    Rigidbody playerRig;
+
+    Vector3 windStrength = new Vector3(-10, 0, 0);
+
+    private void Awake()
+    {
+        player = GameObject.Find("Character");
+        playerScript = player.GetComponent<PlayerController>();
+        playerRig = player.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerScript.enabled = false;
+            playerRig.velocity = transform.rotation * windStrength;
+            //playerScript.enabled = true;
+        }
+    }
+
+}
