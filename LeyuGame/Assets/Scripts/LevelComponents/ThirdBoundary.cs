7292349b992288e322fc62512516268517ac617b
Lenny <lvkuyvenhoven@hotmail.com>
2018-11-30 13:01:56 +0100

    Fourth Boundary in progress
    Progress

--- LeyuGame/Assets/Scripts/LevelComponents/ThirdBoundary.cs
+++ LeyuGame/Assets/Scripts/LevelComponents/ThirdBoundary.cs
@@ -12,6 +12,8 @@ public class ThirdBoundary : MonoBehaviour
     //MANAGEMENT
     bool playerInBoundary;
 
+    public GameObject kube;
+
     [Header("Boundary Settings")]
     public float windAcceleration;
     float windForce;
@@ -22,23 +24,29 @@ public class ThirdBoundary : MonoBehaviour
         playerScript = player.GetComponent<PlayerController>();
     }
 
+    private void Update()
+    {
+        kube.transform.position = transform.position;
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
             windForce += windAcceleration;
             windForce = Mathf.Clamp(windForce, 0, 2);
-            Vector3 windDirection = Quaternion.Inverse(transform.rotation) * (player.transform.position - transform.position);
-            playerScript.boundaryPushingDirection = new Vector3(windDirection.x, 0, windDirection.z).normalized * windForce;
-            playerScript.enablePlayerPushBack = true;
 
             if (playerScript.playerIsAirborne)
             {
                 //player is airborne
+                playerScript.enablePlayerPushBack = false;
             }
             else
             {
                 //player is grounded
+                Vector3 windDirection = Quaternion.Inverse(transform.rotation) * (player.transform.position - transform.position);
+                playerScript.boundaryPushingDirection = new Vector3(windDirection.x, 0, windDirection.z).normalized * windForce;
+                playerScript.enablePlayerPushBack = true;
             }
         }
     }
