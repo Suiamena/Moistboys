654e7a2feeef061e1486f73edef3436622918dd7
Lenny <lvkuyvenhoven@hotmail.com>
2018-12-03 12:43:10 +0100

    Wall fix progress
    ...

--- LeyuGame/Assets/Scenes/InProgress/Lennart's Stash/SmartGoal/NewWallMechanic.cs
+++ LeyuGame/Assets/Scenes/InProgress/Lennart's Stash/SmartGoal/NewWallMechanic.cs
@@ -4,29 +4,53 @@ using UnityEngine;
 
 public class NewWallMechanic : MonoBehaviour {
 
-    [Header("Player Settings")]
+    //PLAYER
     GameObject player;
 
-    [Header("Other Settings")]
+    //CREATURE
     GameObject moustacheBoi;
-    public int creatureRange = 10;
+
+    //UI
+    public GameObject pressButtonPopup;
+
+    [Header("Platform Settings")]
+    GameObject platformsObject;
+    List<GameObject> platforms = new List<GameObject>();
+
+    [Header("Settings")]
+    public int triggerAbilityRange = 10;
 
     private void Awake()
     {
         player = GameObject.Find("Character");
-        moustacheBoi = GameObject.Find("MOD_MoustacheBoi");
+        moustacheBoi = GameObject.Find("Creature");
+        //platformsObject = Resources.FindObjectsOfTypeAll < GameObject.Find("platforms") >;
     }
 
     private void FixedUpdate()
     {
         CutsceneSetup();
+        StartSequence();
     }
 
     void CutsceneSetup()
     {
         //FOLLOW THE PLAYER
-        if (Vector3.Distance(moustacheBoi.transform.position, player.transform.position) < creatureRange)
+        if (Vector3.Distance(moustacheBoi.transform.position, player.transform.position) < triggerAbilityRange)
         {
+            pressButtonPopup.SetActive(true);
+        }
+        else
+        {
+            pressButtonPopup.SetActive(false);
+        }
+    }
+
+    void StartSequence()
+    {
+        if (Input.GetButtonDown("A Button"))
+        {
+            platformsObject.SetActive(true);
         }
     }
 
