7c59e4bc21777544d4ea267a053755e747eb82be
ThomasMeiborg <tp.meiborg@gmail.com>
2018-12-05 15:05:01 +0100

    Cutscene Ontmoeting Creature

---
+++ LeyuGame/Assets/Scenes/InProgress/Thomas' Kleine Croissantjes/CreatureEncounter.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureEncounter : MonoBehaviour {
+
+    GameObject player;
+    PlayerController controllerSwitch;
+    Rigidbody playerBody;
+    GameObject playerModel;
+    Animator playerAnim;
+    public GameObject destructibleBoi;
+    public GameObject cutsceneCamera;
+    GameObject wayPointDraak;
+    GameObject creature;
+    Vector3 distanceToWaypointDraak;
+
+	void Start ()
+    {
+        player = GameObject.Find("Character");
+        creature = GameObject.Find("MOD_Moustacheboi_ANIM_IDLE");
+        controllerSwitch = player.GetComponent<PlayerController>();
+        playerBody = player.GetComponent<Rigidbody>();
+        playerModel = GameObject.Find("MOD_Draak");
+        playerAnim = playerModel.GetComponent<Animator>();
+        wayPointDraak = GameObject.Find("WaypointDraak");
+	}
+	
+    void OnTriggerEnter()
+    {
+        cutsceneCamera.SetActive(true);
+        StartCoroutine(CutsceneTime());
+        controllerSwitch.enabled = false;
+        playerBody.velocity = new Vector3(0, playerBody.velocity.y, 0);
+        if (playerBody.velocity.y > 0)
+        {
+            playerBody.velocity = new Vector3(0, playerBody.velocity.y *-3, 0);
+        }
+        playerAnim.SetBool("IsLaunching",false);
+        //playerAnim.SetBool("IsBouncing", false);
+        playerAnim.SetBool("IsAirborne", false);
+        //player.transform.position = wayPointDraak.transform.position;
+        //player.transform.rotation = Quaternion.Euler(10, wayPointDraak.transform.rotation.y, wayPointDraak.transform.rotation.y);
+    }
+
+    void OnTriggerStay()
+    {
+        player.transform.position = Vector3.MoveTowards(player.transform.position, wayPointDraak.transform.position, 10*Time.deltaTime);
+        player.transform.LookAt(creature.transform.position);
+        distanceToWaypointDraak = player.transform.position - wayPointDraak.transform.position;
+        distanceToWaypointDraak = new Vector3(Mathf.Abs(distanceToWaypointDraak.x), distanceToWaypointDraak.y, distanceToWaypointDraak.z);
+        if (distanceToWaypointDraak.x < 0.5f)
+        {
+            playerAnim.SetBool("IsBouncing", false);
+        }
+    }
+
+    IEnumerator CutsceneTime()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(destructibleBoi);
+
+        yield return new WaitForSeconds(3f);
+        //cutsceneCamera.SetActive(false);
+        //controllerSwitch.enabled = true;
+        //Destroy(gameObject);
+    }
+
+	void Update ()
+    {
+		
+	}
+}
