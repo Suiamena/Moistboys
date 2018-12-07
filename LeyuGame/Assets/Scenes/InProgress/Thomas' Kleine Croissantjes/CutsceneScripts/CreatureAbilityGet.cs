811ef876136789311bed0fbf5df7d0b7e0fec831
ThomasMeiborg <tp.meiborg@gmail.com>
2018-12-07 11:43:53 +0100

    Kutscheme 1

--- LeyuGame/Assets/Scenes/InProgress/Thomas' Kleine Croissantjes/CutsceneScripts/CreatureAbilityGet.cs
+++ LeyuGame/Assets/Scenes/InProgress/Thomas' Kleine Croissantjes/CutsceneScripts/CreatureAbilityGet.cs
@@ -16,12 +16,18 @@ public class CreatureAbilityGet : MonoBehaviour {
 
     Animator creatureAnim;
 
+    GameObject abilityPickUp;
+    Animator abilityAnim;
+    bool movingToCreature = false;
+
     bool camOnCreature = false;
 
     void Start()
     {
         player = GameObject.Find("Character");
         creature = GameObject.Find("MOD_Moustacheboi_ANIM_IDLE");
+        abilityPickUp = GameObject.Find("VFX_Ability_Pickup");
+
         controllerSwitch = player.GetComponent<PlayerController>();
         playerBody = player.GetComponent<Rigidbody>();
         playerModel = GameObject.Find("MOD_Draak");
@@ -29,6 +35,9 @@ public class CreatureAbilityGet : MonoBehaviour {
         wayPointDraak = GameObject.Find("WaypointDraak2");
         creatureAnim = creature.GetComponent<Animator>();
         creatureAnim.SetBool("IsPlaying", false);
+
+        abilityAnim = abilityPickUp.GetComponent<Animator>();
+        abilityAnim.SetBool("IsPlaying", false);
     }
 
     void OnTriggerEnter()
@@ -57,6 +66,7 @@ public class CreatureAbilityGet : MonoBehaviour {
         player.transform.position = Vector3.MoveTowards(player.transform.position, wayPointDraak.transform.position, 10 * Time.deltaTime);
         distanceToWaypointDraak = player.transform.position - wayPointDraak.transform.position;
         distanceToWaypointDraak = new Vector3(Mathf.Abs(distanceToWaypointDraak.x), distanceToWaypointDraak.y, distanceToWaypointDraak.z);
+
         if (distanceToWaypointDraak.x < 0.5f)
         {
             playerAnim.SetBool("IsBouncing", false);
@@ -65,12 +75,25 @@ public class CreatureAbilityGet : MonoBehaviour {
         {
             player.transform.LookAt(creature.transform.position);
         }
+
+        if (movingToCreature == true)
+        {
+        abilityPickUp.transform.position = Vector3.MoveTowards(abilityPickUp.transform.position, creature.transform.position, 5 * Time.deltaTime);
+        abilityPickUp.transform.localScale -= new Vector3(0.008f, 0.008f, 0.008f);
+        abilityPickUp.transform.localScale = new Vector3(Mathf.Clamp(abilityPickUp.transform.localScale.x,0,5), Mathf.Clamp(abilityPickUp.transform.localScale.y, 0, 5), Mathf.Clamp(abilityPickUp.transform.localScale.z, 0, 5));
+        }
     }
 
     IEnumerator CutsceneTime()
     {
         //creatureAnim.SetBool("IsPlaying", true);
         //camOnCreature = true;
+        yield return new WaitForSeconds(1f);
+        abilityAnim.SetBool("IsPlaying", true);
+
+        yield return new WaitForSeconds(1.5f);
+        movingToCreature = true;
+        abilityAnim.enabled = false;
 
         yield return new WaitForSeconds(3f);
 
@@ -80,13 +103,16 @@ public class CreatureAbilityGet : MonoBehaviour {
         //Poging om beweging, waarmee de draak de cutscene in komt, te stoppen wanneer de cutscene afgelopen is.
         playerBody.velocity = new Vector3(0, 0, 0);
         //
-
+        Destroy(abilityPickUp);
         Destroy(gameObject);
     }
 
     void Update()
     {
-        Debug.Log(controllerSwitch.enabled);
+        //if (movingToCreature == true)
+        //{
+        //    abilityPickUp.transform.position = Vector3.MoveTowards(abilityPickUp.transform.position, creature.transform.position, 10 * Time.deltaTime);
+        //}
     }
 
 }
