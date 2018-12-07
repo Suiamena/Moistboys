c8356dcc3d55f68344c49bf10f6646860cec5e2d
Lenny <lvkuyvenhoven@hotmail.com>
2018-12-07 17:11:16 +0100

    Music level 1, 2 and 3 fixed
    Finally! F*ck FMOD

--- LeyuGame/Assets/Scripts/Audio/MusicManagers/Level2MusicManager.cs
+++ LeyuGame/Assets/Scripts/Audio/MusicManagers/Level2MusicManager.cs
@@ -12,17 +12,8 @@ public class Level2MusicManager : MonoBehaviour
 
     void RegulateMusic()
     {
-        StartCoroutine(DoMusic());
-        //PlaySound.musicStage += Mathf.Lerp(0f, 4.5f, 0.005f);
-        //PlaySound.musicStage = Mathf.Clamp(PlaySound.musicStage, 0, 4.5f);
-        //PlaySound.musicStage = 4.5f;
-    }
-
-    IEnumerator DoMusic()
-    {
-        PlaySound.musicStage = 3.5f;
-        yield return new WaitForSeconds(3F);
-        PlaySound.musicStage = 4.5f;
+        ContinuePlayingSound.musicStage += Mathf.Lerp(0f, 4.5f, 0.005f);
+        ContinuePlayingSound.musicStage = Mathf.Clamp(ContinuePlayingSound.musicStage, 0, 4.5f);
     }
 
 }
