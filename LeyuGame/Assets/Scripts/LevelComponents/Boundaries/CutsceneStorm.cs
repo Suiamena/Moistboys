2ea830b0e6e1634e34b2ff57dd4f460d58656788
Lenny <lvkuyvenhoven@hotmail.com>
2018-12-17 20:11:32 +0100

    SnowStorm better

--- LeyuGame/Assets/Scripts/LevelComponents/Boundaries/CutsceneStorm.cs
+++ LeyuGame/Assets/Scripts/LevelComponents/Boundaries/CutsceneStorm.cs
@@ -7,11 +7,23 @@ public class CutsceneStorm : MonoBehaviour {
     public GameObject storm;
     GameObject player;
 
-    bool followPlayer = true;
+    bool followPlayer, startToAccelerate;
+
+    float windStormStrength, particlesSpeed;
+
+    [Header("Particle Settings")]
+    public GameObject snowParticlesWindObject;
+    ParticleSystem snowParticlesSystem;
+    ParticleSystem.EmissionModule emissionModule;
+    ParticleSystem.MainModule main;
 
     private void Awake()
     {
         player = GameObject.Find("Character");
+
+        snowParticlesSystem = snowParticlesWindObject.GetComponent<ParticleSystem>();
+        emissionModule = snowParticlesSystem.emission;
+        main = snowParticlesSystem.main;
     }
 
     private void Update()
@@ -19,6 +31,18 @@ public class CutsceneStorm : MonoBehaviour {
         if (followPlayer)
         {
             storm.transform.position = new Vector3(player.transform.position.x + 10, player.transform.position.y, player.transform.position.z);
+        }
+        if (startToAccelerate)
+        {
+            Debug.Log("hay");
+            windStormStrength += 100F;
+            windStormStrength = Mathf.Clamp(windStormStrength, 0, 5000);
+            emissionModule.rateOverTime = windStormStrength;
+
+            if (windStormStrength == 5000)
+            {
+                startToAccelerate = false;
+            }
         }
     }
 
@@ -28,6 +52,7 @@ public class CutsceneStorm : MonoBehaviour {
         {
             followPlayer = true;
             storm.SetActive(true);
+            startToAccelerate = true;
         }
     }
 
