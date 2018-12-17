dbb249de5ebe0d7a24fc6e6ed9cfdb6e28693081
Lenny <lvkuyvenhoven@hotmail.com>
2018-12-17 17:53:15 +0100

    ResetGame code fixed

--- LeyuGame/Assets/Scripts/ResetGame.cs
+++ LeyuGame/Assets/Scripts/ResetGame.cs
@@ -11,27 +11,47 @@ public class ResetGame : MonoBehaviour {
         {
             AmbienceManager.Ambience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level1Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level2Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level3Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level4Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level5Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level6Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             SceneManager.LoadScene("Level 1");
         }
 
         if (Input.GetKeyDown("2"))
         {
             AmbienceManager.Ambience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level1Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level2Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level3Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level4Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level5Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level6Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             SceneManager.LoadScene("Level 2");
         }
 
         if (Input.GetKeyDown("3"))
         {
             AmbienceManager.Ambience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level1Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level2Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level3Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level4Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level5Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level6Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             SceneManager.LoadScene("Level 3");
         }
 
         if (Input.GetKeyDown("4"))
         {
             AmbienceManager.Ambience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level1Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level2Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level3Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level4Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level5Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level6Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             SceneManager.LoadScene("level 4");
         }
 
@@ -39,13 +59,22 @@ public class ResetGame : MonoBehaviour {
         {
             AmbienceManager.Ambience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level1Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level2Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level3Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level4Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level5Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level6Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             SceneManager.LoadScene("Level 5");
         }
 
         if (Input.GetKeyDown("6"))
         {
             AmbienceManager.Ambience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level1Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level2Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level3Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level4Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level5Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level6Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             SceneManager.LoadScene("Level 6");
         }
@@ -53,6 +82,11 @@ public class ResetGame : MonoBehaviour {
         if (Input.GetKeyDown("7"))
         {
             AmbienceManager.Ambience.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level1Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level2Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level3Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level4Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            Level5Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             Level6Music.Music.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             SceneManager.LoadScene("TitleScreen");
         }
