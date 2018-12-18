ad3c3b720375b039bb07a0def1bea4620a5bcc9a
Lenny <lvkuyvenhoven@hotmail.com>
2018-12-18 15:35:09 +0100

    Fix

--- LeyuGame/Assets/Scripts/GameArchitecture/TitleScreen.cs
+++ LeyuGame/Assets/Scripts/GameArchitecture/TitleScreen.cs
@@ -73,6 +73,7 @@ public class TitleScreen : MonoBehaviour {
         {
             if (iconPosition == 1)
             {
+                Time.timeScale = 1;
                 SceneManager.LoadScene("Level 1");
             }
             if (iconPosition == 2)
