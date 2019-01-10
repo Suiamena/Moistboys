188d536f38f1c413d9cb7840974d02dd00bb9b88
Lenny <lvkuyvenhoven@hotmail.com>
2019-01-10 12:18:11 +0100

    Level 5 test

--- LeyuGame/Assets/Scenes/InProgress/Lennart's Stash/RotationTest.cs
+++ LeyuGame/Assets/Scenes/InProgress/Lennart's Stash/RotationTest.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class RotationTest : MonoBehaviour {
 
     public GameObject playerModel;
+    public GameObject playerCharacter;
 
     private void Awake()
     {
-        StartCoroutine(Test());
+        StartCoroutine(TestCharacter());
     }
 
     IEnumerator Test()
+    {
+        yield return new WaitForSeconds(1f);
+        playerModel.transform.rotation = Quaternion.Euler(0, 90, 0);
+        yield return new WaitForSeconds(1f);
+        playerModel.transform.rotation = Quaternion.Euler(0, 180, 0);
+        yield return new WaitForSeconds(1f);
+        playerModel.transform.rotation = Quaternion.Euler(0, 270, 0);
+    }
+
+    IEnumerator TestCharacter()
     {
         yield return new WaitForSeconds(1f);
         playerModel.transform.rotation = Quaternion.Euler(0, 90, 0);
