43e18ee8a75e12bb102286d539f98edecf973df3
Lenny <lvkuyvenhoven@hotmail.com>
2019-01-16 16:45:46 +0100

    Wall progress

---
+++ LeyuGame/Assets/Scripts/LevelComponents/CreatureWall/InspectorElevatorSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectorElevatorSpeed : MonoBehaviour {
+
+    public int setElevatorSpeed = 15;
+    public GameObject elevatorObject;
+    ElevatorPlatform elevatorScript;
+
+    private void Awake()
+    {
+        elevatorScript = elevatorObject.GetComponent<ElevatorPlatform>();
+    }
+
+    private void Update()
+    {
+        elevatorScript.elevatorSpeed = setElevatorSpeed;
+    }
+
+}
