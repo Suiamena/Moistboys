9cf011e5e574babfc6fd47831d6164c62899f4ce
Lenny <lvkuyvenhoven@hotmail.com>
2019-01-16 12:52:30 +0100

    New Interactive Wall

---
+++ LeyuGame/Assets/Scripts/LevelComponents/CreatureWall/MeshRendererInactive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshRendererInactive : MonoBehaviour {
+
+    MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+}
