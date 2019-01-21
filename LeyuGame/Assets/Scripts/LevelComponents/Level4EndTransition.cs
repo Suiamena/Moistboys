2460a2561ca010249f2518bebbe68a498c93bda6
MikouZonata <kevin.vanas@student.hku.nl>
2019-01-21 15:30:23 +0100

    asdasdasdasd

--- LeyuGame/Assets/Scripts/LevelComponents/Level4EndTransition.cs
+++ LeyuGame/Assets/Scripts/LevelComponents/Level4EndTransition.cs
@@ -7,6 +7,7 @@ public class Level4EndTransition : MonoBehaviour
 {
 	public float transitionLength = 2;
 	public PostProcessVolume beforePostProcessing, afterPostProcessing;
+	float postProcessingRate;
 	public Renderer[] renderers;
 	List<Material> materials;
 	bool running = false;
@@ -15,6 +16,7 @@ public class Level4EndTransition : MonoBehaviour
 	{
 		beforePostProcessing.weight = 1;
 		afterPostProcessing.weight = 0;
+		postProcessingRate = 1 / transitionLength;
 	}
 
 	public IEnumerator Transition ()
@@ -22,6 +24,7 @@ public class Level4EndTransition : MonoBehaviour
 		List<float> transitionRate = new List<float>(), transitionFill = new List<float>();
 		materials = new List<Material>();
 
+		//RETRIEVE CORRECT MATERIALS
 		for (int i = 0; i < renderers.Length; ++i) {
 			foreach (Renderer r in renderers) {
 				foreach (Material m in r.materials) {
@@ -31,18 +34,26 @@ public class Level4EndTransition : MonoBehaviour
 			}
 		}
 
+		//RETRIEVE TRANSITION VALUES
 		for (int i = 0; i < materials.Count; i++) {
 			transitionFill.Add(materials[i].GetFloat("_SurfaceSpreadTop"));
 			transitionRate.Add(transitionFill[i] / transitionLength);
 		}
 
+		//DO THINGS OVER TIME
 		for (float t = 0; t < transitionLength; t += Time.deltaTime) {
 			for (int i = 0; i < materials.Count; ++i) {
 				transitionFill[i] = Mathf.MoveTowards(transitionFill[i], 0, transitionRate[i] * Time.deltaTime);
 				materials[i].SetFloat("_SurfaceSpreadTop", transitionFill[i]);
 			}
+
+			beforePostProcessing.weight += -postProcessingRate * Time.deltaTime;
+			afterPostProcessing.weight += postProcessingRate * Time.deltaTime;
 			yield return null;
 		}
+
+		beforePostProcessing.weight = 0;
+		afterPostProcessing.weight = 1;
 
 		foreach (Renderer r in renderers)
 			r.transform.tag = "Rock";
