59de4c434d286388edbe9e5f28ffe7c33ea5d0de
iekeby <iekeby@hotmail.com>
2019-01-22 11:46:58 +0100

    angery wie heeft mijn snowfloor gewoon deadass weggegooid >:(((

--- LeyuGame/Assets/Scripts/LevelComponents/Level4EndTransition.cs
+++ LeyuGame/Assets/Scripts/LevelComponents/Level4EndTransition.cs
@@ -5,57 +5,72 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class Level4EndTransition : MonoBehaviour
 {
-	public float transitionLength = 2;
-	public PostProcessVolume beforePostProcessing, afterPostProcessing;
-	float postProcessingRate;
-	public Renderer[] renderers;
-	List<Material> materials;
-	bool running = false;
+    public float transitionLength = 2;
+    public PostProcessVolume beforePostProcessing, afterPostProcessing;
+    float postProcessingRate;
+    public Renderer[] renderers;
+    List<Material> materials;
+    bool running = false;
 
-	private void Awake ()
-	{
-		beforePostProcessing.weight = 1;
-		afterPostProcessing.weight = 0;
-		postProcessingRate = 1 / transitionLength;
-	}
+    private void Awake()
+    {
+        beforePostProcessing.weight = 1;
+        afterPostProcessing.weight = 0;
+        postProcessingRate = 1 / transitionLength;
+    }
 
-	public IEnumerator Transition ()
-	{
-		List<float> transitionRate = new List<float>(), transitionFill = new List<float>();
-		materials = new List<Material>();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !running)
+        {
+            running = true;
+            StartCoroutine(Transition());
+        }
+    }
 
-		//RETRIEVE CORRECT MATERIALS
-		for (int i = 0; i < renderers.Length; ++i) {
-			foreach (Renderer r in renderers) {
-				foreach (Material m in r.materials) {
-					if (m.HasProperty("_SurfaceSpreadTop"))
-						materials.Add(m);
-				}
-			}
-		}
+    public IEnumerator Transition()
+    {
+        List<float> transitionRate = new List<float>(), transitionFill = new List<float>();
+        materials = new List<Material>();
 
-		//RETRIEVE TRANSITION VALUES
-		for (int i = 0; i < materials.Count; i++) {
-			transitionFill.Add(materials[i].GetFloat("_SurfaceSpreadTop"));
-			transitionRate.Add(transitionFill[i] / transitionLength);
-		}
+        //RETRIEVE CORRECT MATERIALS
+        for (int i = 0; i < renderers.Length; ++i)
+        {
+            foreach (Renderer r in renderers)
+            {
+                foreach (Material m in r.materials)
+                {
+                    if (m.HasProperty("_SurfaceSpreadTop"))
+                        materials.Add(m);
+                }
+            }
+        }
 
-		//DO THINGS OVER TIME
-		for (float t = 0; t < transitionLength; t += Time.deltaTime) {
-			for (int i = 0; i < materials.Count; ++i) {
-				transitionFill[i] = Mathf.MoveTowards(transitionFill[i], 0, transitionRate[i] * Time.deltaTime);
-				materials[i].SetFloat("_SurfaceSpreadTop", transitionFill[i]);
-			}
+        //RETRIEVE TRANSITION VALUES
+        for (int i = 0; i < materials.Count; i++)
+        {
+            transitionFill.Add(materials[i].GetFloat("_SurfaceSpreadTop"));
+            transitionRate.Add(transitionFill[i] / transitionLength);
+        }
 
-			beforePostProcessing.weight += -postProcessingRate * Time.deltaTime;
-			afterPostProcessing.weight += postProcessingRate * Time.deltaTime;
-			yield return null;
-		}
+        //DO THINGS OVER TIME
+        for (float t = 0; t < transitionLength; t += Time.deltaTime)
+        {
+            for (int i = 0; i < materials.Count; ++i)
+            {
+                transitionFill[i] = Mathf.MoveTowards(transitionFill[i], 0, transitionRate[i] * Time.deltaTime);
+                materials[i].SetFloat("_SurfaceSpreadTop", transitionFill[i]);
+            }
 
-		beforePostProcessing.weight = 0;
-		afterPostProcessing.weight = 1;
+            beforePostProcessing.weight += -postProcessingRate * Time.deltaTime;
+            afterPostProcessing.weight += postProcessingRate * Time.deltaTime;
+            yield return null;
+        }
 
-		foreach (Renderer r in renderers)
-			r.transform.tag = "Rock";
-	}
+        beforePostProcessing.weight = 0;
+        afterPostProcessing.weight = 1;
+
+        foreach (Renderer r in renderers)
+            r.transform.tag = "Rock";
+    }
 }
