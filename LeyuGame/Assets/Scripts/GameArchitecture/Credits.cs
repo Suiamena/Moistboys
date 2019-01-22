0e1a1a0b4369c514b049e1d2386129850b3d2598
Lenny <lvkuyvenhoven@hotmail.com>
2019-01-22 12:38:47 +0100

    Credits Placeholders

---
+++ LeyuGame/Assets/Scripts/GameArchitecture/Credits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+    private void Awake()
+    {
+        StartCoroutine(LoadTitleScreen());
+    }
+
+    IEnumerator LoadTitleScreen()
+    {
+        yield return new WaitForSeconds(5f);
+        Debug.Log("load");
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+}
