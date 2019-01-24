561d2a9259081d5ca94055ca71645381554c3a0f
Lenny <lvkuyvenhoven@hotmail.com>
2019-01-24 17:18:26 +0100

    Credits done

---
+++ LeyuGame/Assets/Scripts/Audio/MusicManagers/CreditsMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMusic : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string music = "event:/Music";
+    public static FMOD.Studio.EventInstance Music;
+    public static FMOD.Studio.ParameterInstance MusicParameter;
+
+    public static float musicStage;
+
+    private void Awake()
+    {
+        Music = FMODUnity.RuntimeManager.CreateInstance(music);
+        Music.getParameter("Music", out MusicParameter);
+
+        Music.start();
+
+        musicStage = 15.0f;
+        MusicParameter.setValue(musicStage);
+        Debug.Log(musicStage);
+    }
+}
