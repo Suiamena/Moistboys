b96c4c5bfca7cfa21d735034d7e586c1e770f04f
MikouZonata <kevin.vanas@student.hku.nl>
2019-01-31 15:02:15 +0100

    lvl 4 end transition

--- LeyuGame/Assets/Scripts/LevelComponents/Level4EndTransition.cs
+++ LeyuGame/Assets/Scripts/LevelComponents/Level4EndTransition.cs
@@ -9,21 +9,34 @@ public class Level4EndTransition : MonoBehaviour
     public PostProcessVolume beforePostProcessing, afterPostProcessing;
     float postProcessingRate;
     public Renderer[] renderers;
-    List<Material> materials;
-    bool running = false;
+	List<Material> materials = new List<Material>();
+	List<float> transitionRate = new List<float>(), transitionFill = new List<float>();
+	bool running = false;
 
     private void Awake()
     {
         beforePostProcessing.weight = 1;
         afterPostProcessing.weight = 0;
         postProcessingRate = 1 / transitionLength;
-    }
 
-    // loooool
-    //private void OnEnable()
-    //{
-    //    Transition();
-    //}
+		//RETRIEVE CORRECT MATERIALS
+		for (int i = 0; i < renderers.Length; ++i) {
+			foreach (Renderer r in renderers) {
+				foreach (Material m in r.materials) {
+					if (m.HasProperty("_SurfaceSpreadTop")) {
+						materials.Add(m);
+						m.SetFloat("_SurfaceSpreadTop", 0);
+					}
+				}
+			}
+		}
+
+		//RETRIEVE TRANSITION VALUES
+		for (int i = 0; i < materials.Count; i++) {
+			transitionFill.Add(materials[i].GetFloat("_SurfaceSpreadTop"));
+			transitionRate.Add(transitionFill[i] / transitionLength);
+		}
+	}
 
 	public void Transition ()
 	{
@@ -36,29 +49,6 @@ public class Level4EndTransition : MonoBehaviour
 
     public IEnumerator TransitionRoutine()
     {
-        List<float> transitionRate = new List<float>(), transitionFill = new List<float>();
-        materials = new List<Material>();
-
-        //RETRIEVE CORRECT MATERIALS
-        for (int i = 0; i < renderers.Length; ++i)
-        {
-            foreach (Renderer r in renderers)
-            {
-                foreach (Material m in r.materials)
-                {
-                    if (m.HasProperty("_SurfaceSpreadTop"))
-                        materials.Add(m);
-                }
-            }
-        }
-
-        //RETRIEVE TRANSITION VALUES
-        for (int i = 0; i < materials.Count; i++)
-        {
-            transitionFill.Add(materials[i].GetFloat("_SurfaceSpreadTop"));
-            transitionRate.Add(transitionFill[i] / transitionLength);
-        }
-
         //DO THINGS OVER TIME
         for (float t = 0; t < transitionLength; t += Time.deltaTime)
         {
